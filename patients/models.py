5d55fb58746ce8677dea7b128af3fc8b9857e687
duran tchuente <duran.tchuente@gmail.com>
2024-10-11 10:43:01 -0300

    change field

--- patients/models.py
+++ patients/models.py
@@ -4,12 +4,12 @@ class Patient(models.Model):
     nom_complet = models.CharField(max_length=255)
     date_naissance = models.DateField()
     SEXE_CHOICES = [
-        ('M', 'Homme'),
-        ('F', 'Femme'),
+        (0, 'Homme'),
+        (1, 'Femme')
     ]
-    sexe = models.CharField(max_length=1, choices=SEXE_CHOICES)
+    sexe = models.IntegerField(max_length=1, choices=SEXE_CHOICES)
     adresse = models.TextField()
-    telephone = models.CharField(max_length=15)
+    telephone = models.PositiveIntegerField(max_length=15)
     email = models.EmailField()
     GROUPE_SANGUIN_CHOICES = [
         ('A+', 'A+'),
@@ -23,7 +23,8 @@ class Patient(models.Model):
     ]
     groupe_sanguin = models.CharField(max_length=3, choices=GROUPE_SANGUIN_CHOICES)
     date_enregistrement = models.DateTimeField(auto_now_add=True)
-    historique_medical = models.TextField(blank=True, null=True)
     consentement = models.BooleanField(default=False)
+    medicalHistoryLink = models.URLField()
+    bodyTemperature = models.FloatField()
     def __str__(self):
         return self.nom_complet
